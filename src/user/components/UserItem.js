b3666c2819818fb5024d73cee5279b3e6f56eeb0
mayankguptamkt1996 <mayankgupta.mkt1996@gmail.com>
2025-01-10 00:23:56 +0530

    added the card view

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './UserItem.css'
+import Avatar from '../../shared/UIelements/Avatar'
+import Card from '../../shared/UIelements/Card'
+import { Link } from 'react-router';
 export default function UserItem(props) {
   return (
     <li className='user-item'>
-      <div className='user-item_content'>
+      <Card className ="user-item_content">
+        <Link to={`/${props.id}/places`}>
     <div className="user-item-image">
-        <img src={props.place} alt ={props.name}/>
+      <Avatar image={props.place} alt={props.name}/>
     </div>
     <div className="user-item_info">
         <h2>{props.name}</h2>
         <h3>{props.placeCount} {props.placeCount===1?'Place':'Places'}</h3>
     </div>
-      </div>
+    </Link>
+      </Card>
     </li>
   );
 }
